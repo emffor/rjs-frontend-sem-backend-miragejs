a7122904f77df5a120bc8b953e176364895d823f
E Ferreira <98027423+emffor@users.noreply.github.com>
2022-04-30 17:38:18 -0300

    buscando api com UseState

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,29 +1,35 @@
 import { Transactions } from '../../components/Transactions';
 import styles from './styles.module.scss';
+import { useEffect, useState } from 'react';
 
 interface Transactions  {
+  id: string;
   title: string;
   amount: number;
   category: string;
   createdAt: string;
 }
 
-const transactions = {
-  title: 'Transaction 1',
-  amount: 1000,
-  category: 'Food',
-  createdAt: new Date().toISOString()
-}
-
 export function Home(){
- 
+  const [transactions, setTransactions] = useState<Transactions[]>([]);
+
+  
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+    .then(response => response.json())
+    .then(data => setTransactions(data));
+  },[]);
+
+
   return (
     <main className={styles.container}>
         <h1>PAGE HOME <span>MIRAGE JS</span></h1>
 
-        <Transactions 
-          data={transactions}
-        />
+        {
+          transactions.map(transaction => {
+            return <Transactions key={transaction.id} data={transaction} />
+          })
+        }
     </main>
   );
 }
