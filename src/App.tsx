e203474f6e7ce9f7a22d29b7fdab65b8164103cb
E Ferreira <98027423+emffor@users.noreply.github.com>
2022-04-30 16:56:06 -0300

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { useEffect } from 'react';
+
 import { Home } from './pages/Home';
-import { RepositoryList } from './pages/RepositoryList';
 import './styles/global.scss';
 
-function App () {
+export function App () {
+
+  useEffect(() => {
+    fetch('hhttp://localhost:3000/api/transactions')
+    .then(response => response.json())
+    .then(data => console.log(data));
+    
+  },[]);
+
   return (
     <>
-      <RepositoryList />
+      <Home />
     </>
   );
 }
 
-export default App;
+
