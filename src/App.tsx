fb4988124e98830e35f067021a7878be87051c5e
E Ferreira <98027423+emffor@users.noreply.github.com>
2022-04-30 17:11:07 -0300

    add: CreateServer MirageJS fake

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,9 @@ import './styles/global.scss';
 export function App () {
 
   useEffect(() => {
-    fetch('hhttp://localhost:3000/api/transactions')
+    fetch('http://localhost:3000/api/transactions')
     .then(response => response.json())
     .then(data => console.log(data));
-    
   },[]);
 
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api'; //a a partir do http://localhost:3000/api/...
+    this.get('/transactions' ,() => {
+      return [
+        { 
+          id: 'a1s1e1s25s1e1s2', 
+          title: 'Transaction 1',
+          amount: 1000,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date().toISOString()
+        },
+        {
+          id: '8ae098eauieaiu', 
+          title: 'Transaction 2',
+          amount: 2500,
+          type: 'exit',
+          category: 'Food',
+          createdAt: new Date().toISOString()
+        },
+        {
+          id: '4556456ggtt5555', 
+          title: 'Transaction 3',
+          amount: 5000,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date().toISOString()
+        },
+      ]
+    });
+  }
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
